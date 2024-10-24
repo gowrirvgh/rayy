e436b9d57952426c3dd2f0fc402200e3df95a626
gowrirvgh <gowrivijayakumar86@gmail.com>
2024-10-24 12:46:15 +0530

    Update and rename test.py to test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Enter a number:"
+read num1
+echo "Enter another number:"
+read num2
+if [[ $num2 -gt $num2 ]];
+echo "The first number is great"
+else
+echo "The second number is great"
